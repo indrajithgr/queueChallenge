96bd14ad28c69efd2572a167645d11ca2ccf07e8
indrajith <ijithgr@gmail.com>
2019-08-07 12:27:41 +0400

    first commit

---
+++ src/main/java/com/util/immutablequeue/App.java
@@ -0,0 +1,12 @@
+package com.util.immutablequeue;
+
+/**
+ * Immutable Queue
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Immutable Queue!" );
+    }
+}
---
+++ src/main/java/com/util/immutablequeue/impl/ImmutableQueue.java
@@ -0,0 +1,33 @@
+package com.util.immutablequeue.impl;
+
+import com.util.immutablequeue.api.Queue;
+
+/**
+ * Purpose ->
+ * @author Indrajith G R Aug 7, 2019 12:18:47 PM
+ * @version 1.0.0.0
+ * @param <T>
+ */
+public class ImmutableQueue<T> implements Queue<T> {
+
+	public Queue<T> enQueue(T t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Queue<T> deQueue(T t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public T head() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
